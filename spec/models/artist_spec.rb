31a950cfd6f1ea996c46cc66f33862af00ce1987
Matt Deming <vballmatt44@gmail.com>
2022-05-11 20:13:20 -0600

    Adds model testing, average_song_length method

---
+++ spec/models/artist_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Artist do
+  it {should have_many :songs}
+
+  describe "instance methods" do
+    describe "#average_song_length" do
+      before :each do
+        @zac_brown_band = Artist.create!(name: "Zac Brown Band")
+        @toes = @zac_brown_band.songs.create!(title: "Toes", length: 345, play_count: 7654321)
+        @colorado = @zac_brown_band.songs.create!(title: "Colorado", length: 825, play_count: 9814256)
+        @truckin = @zac_brown_band.songs.create!(title: "Truckin", length: 1, play_count: 42)
+
+      end
+
+      it "returns the average song length" do
+        expect(@zac_brown_band.average_song_length.round(2)).to eq(390.33)
+      end
+    end
+  end
+end
